8521bab7e4c3500b4c0fea83bb3fe2cb8d859d1c
C4nerr <vershu2007@gmail.com>
2026-06-04 18:33:49 +0300

    Tokenizer.cpp + Tokenizer.h = done

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -3,3 +3,76 @@
 //
 
 #include "Tokenizer.h"
+
+Tokenizer::Tokenizer() {
+    stop_words.insert({"and", "a", "in", "the"});
+}
+
+Tokenizer::~Tokenizer() = default;
+
+std::vector<std::string> Tokenizer::Tokenize(const std::string &input) {
+    const std::string temp1 = Lowercasing(input);
+    const std::string temp2 = Punctuations_Removal(temp1);
+    std::vector<std::string> temp = Splitting(temp2);
+    std::vector<std::string> result = Stop_Words_Removal(temp);
+    return result;
+}
+
+std::string Tokenizer::Punctuations_Removal(const std::string &input) { //NOLINT
+    std::string result;
+    //auto result = " ";
+    for (const char & i : input) {
+        if (i == ',' || i == '!' || i == '?' || i == '.' || i == '/' || i == '(' || i == ')' || i == '[' || i == ']' || i == '{' || i == '}') {
+            continue;
+        }
+        result += i;
+    }
+    return result;
+}
+
+std::vector<std::string> Tokenizer::Splitting(const std::string &input) { //NOLINT
+    std::vector<std::string> result;
+    std::string temp;
+    for (const char & i : input) {
+        if (!(i == ' ' || i == '\n' || i == '\t')) {
+            temp += i;
+        }
+        else {
+            if (!(temp.empty())) {
+                result.push_back(temp);
+                temp.clear();
+            }
+        }
+    }
+    if (!temp.empty()) {
+        result.push_back(temp);
+        temp.clear();
+    }
+    return result;
+}
+
+std::vector<std::string> Tokenizer::Stop_Words_Removal(const std::vector<std::string> &input) { //NOLINT
+    std::vector<std::string> result;
+    for (auto & i : input) {
+        if (stop_words.contains(i)) {
+            continue;
+        }
+        result.push_back(i);
+    }
+    return result;
+}
+
+
+std::string Tokenizer::Lowercasing(const std::string &input) { //NOLINT
+    std::string result;
+    for (const char & i : input) {
+        if (i >= 'A' && i <= 'Z') {
+            result.push_back(tolower(i)); //Clang-Tidy: Narrowing conversion from 'int' to signed type 'char' is implementation-defined
+        }
+        else {
+            result.push_back(i);
+        }
+    }
+    return result;
+}
+
--- Tokenizer.h
+++ Tokenizer.h
@@ -4,9 +4,23 @@
 
 #ifndef BM25_C___TOKENIZER_H
 #define BM25_C___TOKENIZER_H
+#include <string>
+#include <unordered_set>
+#include <vector>
 
 
 class Tokenizer {
+    public:
+    Tokenizer();
+    ~Tokenizer();
+    std::vector<std::string> Tokenize(const std::string& input);
+
+    private:
+    std::string Punctuations_Removal(const std::string& input); //убрать точки, запятые и тд
+    std::vector<std::string> Splitting(const std::string& input); //порезать текст на куски
+    std::vector<std::string> Stop_Words_Removal(const std::vector<std::string>& input); //выкидка предлогов и союзов ("и", "в", "а", "на" и тд)
+    std::string Lowercasing(const std::string &input); // привести все к нижнему регистру (ЯБЛОКО/Яблоко -> яблоко)
+    std::unordered_set<std::string> stop_words;
 };
 
 
